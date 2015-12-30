6696c421705e2b79ad8fe052ded8f0eb81d9c88f
Diana Shkolnikov <dianashk@gmail.com>
2015-12-29 19:47:01 -0500

    switch tracks

--- MapzenSearchBot.js
+++ MapzenSearchBot.js
@@ -17,18 +17,23 @@ util.inherits(MapzenSearchBot, Bot);
 MapzenSearchBot.prototype.run = function () {
   MapzenSearchBot.super_.call(this, this.settings);
 
+  console.log('run', this.settings);
   this.on('start', this._onStart);
   this.on('message', this._onMessage);
 };
 
 MapzenSearchBot.prototype._onStart = function () {
+  console.log('onStart');
+
   var self = this;
   this.user = this.users.filter(function (user) {
+    console.log(user.name);
     return user.name === self.name;
   })[0];
 };
 
 MapzenSearchBot.prototype._onMessage = function (message) {
+  console.log('message', JSON.stringify(message, null, 2));
   if (this._isChatMessage(message) &&
     this._isChannelConversation(message) &&
     !this._isFromMapzenSearchBot(message) &&
@@ -39,28 +44,38 @@ MapzenSearchBot.prototype._onMessage = function (message) {
 };
 
 MapzenSearchBot.prototype._isChatMessage = function (message) {
-  return message.type === 'message' && Boolean(message.text);
+  var res = message.type === 'message' && Boolean(message.text);
+  console.log('isChat:', res);
+  return res;
 };
 
 MapzenSearchBot.prototype._isChannelConversation = function (message) {
-  return typeof message.channel === 'string' &&
-    message.channel[0] === 'C';
+  var res = typeof message.channel === 'string';
+  console.log('isChannelConversation:', res);
+  return res;
 };
 
 MapzenSearchBot.prototype._isFromMapzenSearchBot = function (message) {
-  return message.user === this.user.id;
+  var res = message.user === this.user.id;
+  console.log('isUserId:', res);
+  return res;
 };
 
 MapzenSearchBot.prototype._isMentioningSearch = function (message) {
-  return message.text.toLowerCase().indexOf('/search') > -1 ||
-    message.text.toLowerCase().indexOf('/autocomplete') > -1;
+  var res =  message.text.toLowerCase().indexOf('mapzen_search') > -1 ||
+    message.text.toLowerCase().indexOf('mapzen_autocomplete') > -1;
+  console.log('isSearchMsg:', res);
+  return res;
 };
 
 MapzenSearchBot.prototype._replyWithResults = function (originalMessage) {
   var self = this;
 
   var url = 'https://search.mapzen.com/v1/search?api_key=' + self.settings.api_key + '&' + originalMessage.text;
+  console.log('url', url);
   request.get(url, function (err, res) {
+    console.log(err, res.response);
+
     var channel = self._getChannelById(originalMessage.channel);
     self.postMessageToChannel(
       channel.name,
@@ -71,6 +86,7 @@ MapzenSearchBot.prototype._replyWithResults = function (originalMessage) {
 };
 
 MapzenSearchBot.prototype._getChannelById = function (channelId) {
+  console.log('channels:', this.channels);
   return this.channels.filter(function (item) {
     return item.id === channelId;
   })[0];
--- app.js
+++ app.js
@@ -1,15 +1,44 @@
 'use strict';
 
-var SearchBot = require('./MapzenSearchBot');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
-var token = process.env.BOT_API_KEY || 'xoxb-17533781585-CCRUdOWVNyz50Ik4IMyzYn9D';
-var name = process.env.BOT_NAME || 'mapzen_search';
 var api_key = process.env.SEARCH_API_KEY || 'search-raxpuHw';
 
-var bot = new SearchBot({
-  token: token,
-  name: name,
-  api_key: api_key
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+app.post('/search', function(req, res) {
+  /*
+   token=PJ4OdQwofdlBK2WUbigefgOL
+   team_id=T0001
+   team_domain=example
+   channel_id=C2147483705
+   channel_name=test
+   user_id=U2147483697
+   user_name=Steve
+   command=/weather
+   text=94070
+   response_url=https://hooks.slack.com/commands/1234/5678
+   */
+
+  if(!req.body.hasOwnProperty('text')){
+    res.statusCode = 400;
+    return res.send('Error 400: Post syntax incorrect.');
+  }
+
+  var url = 'https://search.mapzen.com/v1/search?api_key=' + api_key + '&' + req.body.text;
+  console.log('url', url);
+  request.get(url, function (err, results) {
+    console.log(err, results);
+    res.json(results);
+  });
+
 });
 
-bot.run();
+app.listen(3000);
